8be9ecf711acaf3fbb5fcfd7706a93c32042fdf4
vonaxl <vonaxlcorso@gmail.com>
2019-10-28 14:52:53 +0100

    first one done

--- palindroma/assets/js/myscript.js
+++ palindroma/assets/js/myscript.js
@@ -1,2 +1,18 @@
 
-document.getElementById('n1out').innerHTML = n1;
+// document.getElementById('n1out').innerHTML = n1;
+// Una funzione per capire se la parola è palindroma
+
+var x = prompt("Inserisci la parola : ");
+console.log(x);
+var verifica = palindroma();
+
+
+function palindroma() {
+     var y = x.split("").reverse().join("") ;
+     console.log(y);
+     if (x==y) {
+         console.log("E' palindroma");
+     }else{
+         console.log("Non è palindroma");     
+     }
+}

f2b4213d8c2d20b418a85edfcf807f3126fbf06a
vonaxl <vonaxlcorso@gmail.com>
2019-10-28 15:42:27 +0100

    html output of palindroma done

--- palindroma/assets/js/myscript.js
+++ palindroma/assets/js/myscript.js
@@ -4,15 +4,25 @@
 
 var x = prompt("Inserisci la parola : ");
 console.log(x);
+document.getElementById('xOut').innerHTML = x;
 var verifica = palindroma();
 
 // prima separiamo la parola , cambiamo le posizioni delle lettere e li mettiamo insieme
 function palindroma() {
      var y = x.split("").reverse().join("") ;
      console.log(y);
+     document.getElementById('yOut').innerHTML = y;
      if (x==y) {
          console.log("E' palindroma");
+         return true
      }else{
-         console.log("Non è palindroma");     
+         console.log("Non è palindroma");  
+        return false
      }
+}
+
+if (verifica==true) {
+    document.getElementById('zOut').innerHTML = "La parola è palindroma";
+} else {
+    document.getElementById('zOut').innerHTML = "La parola non è palindroma";
 }

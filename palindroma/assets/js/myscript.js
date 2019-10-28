c08246b6fcd731ab934dd6ca82f898b7a49d3d69
vonaxl <vonaxlcorso@gmail.com>
2019-10-28 18:18:01 +0100

    changed word using only for

--- palindroma/assets/js/myscript.js
+++ palindroma/assets/js/myscript.js
@@ -5,21 +5,44 @@
 var x = prompt("Inserisci la parola : ");
 console.log(x);
 document.getElementById('xOut').innerHTML = x;
+
+// if you don't write the y=""; it will show undefined + result
+
+var y="";
+// We start from counting the length of the word then subtracting one and then we start from the last and save each word by each round
+for (var i = x.length -1; i>=0; i--){
+    y += x[i];
+}
+console.log(y);
+document.getElementById('yOut').innerHTML = y;
+
+
 var verifica = palindroma();
+function palindroma() {
+
+    if (x==y) {
+        console.log("E' palindroma");
+        return true
+    }else{
+       console.log("Non è palindroma");  
+       return false
+    }
+}
+
 
 // prima separiamo la parola , cambiamo le posizioni delle lettere e li mettiamo insieme
-function palindroma() {
-     var y = x.split("").reverse().join("") ;
-     console.log(y);
-     document.getElementById('yOut').innerHTML = y;
-     if (x==y) {
-         console.log("E' palindroma");
-         return true
-     }else{
-         console.log("Non è palindroma");  
-        return false
-     }
-}
+// function palindroma() {
+//      var y = x.split("").reverse().join("") ;
+//      console.log(y);
+//      document.getElementById('yOut').innerHTML = y;
+//      if (x==y) {
+//          console.log("E' palindroma");
+//          return true
+//      }else{
+//          console.log("Non è palindroma");  
+//         return false
+//      }
+// }
 
 if (verifica==true) {
     document.getElementById('zOut').innerHTML = "La parola è palindroma";
